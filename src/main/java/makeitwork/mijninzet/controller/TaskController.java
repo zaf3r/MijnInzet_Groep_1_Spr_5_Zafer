1071d0d0ced45a9315621d914d027d27d194d6d2
PeterP <peter.post@hva.nl>
2019-05-24 22:22:42 +0200

    sorteren vacaturen

--- src/main/java/makeitwork/mijninzet/controller/TaskController.java
+++ src/main/java/makeitwork/mijninzet/controller/TaskController.java
@@ -2,15 +2,23 @@ package makeitwork.mijninzet.controller;
 
 
 import makeitwork.mijninzet.model.Task;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import makeitwork.mijninzet.repository.TaskRepository;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+import static java.util.Comparator.naturalOrder;
 
 @Controller
 public class TaskController {
 
+    @Autowired
     private TaskRepository taskRepository;
 
     public TaskController(TaskRepository taskRepository){
@@ -20,6 +28,7 @@ public class TaskController {
     @GetMapping("/taskOverview") //th:action
     public String MenuHandler(Model model){
         List<Task> tasks = this.taskRepository.findAll();
+        Collections.sort(tasks,(a,b) -> {return a.getTitel().compareTo(b.getTitel());});
         model.addAttribute("allTasks", tasks);
         return "taskOverview";
     }
