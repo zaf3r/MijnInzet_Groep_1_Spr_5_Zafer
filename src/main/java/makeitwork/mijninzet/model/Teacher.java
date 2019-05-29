b8802c806a3d12f4543258dce562ce643acaf17d
Merel <m.slingenberg@hotmail.nl>
2019-05-29 14:57:39 +0200

    gezeik

--- src/main/java/makeitwork/mijninzet/model/Teacher.java
+++ src/main/java/makeitwork/mijninzet/model/Teacher.java
@@ -1,32 +1,32 @@
 package makeitwork.mijninzet.model;
 
 import javax.persistence.*;
-import java.util.List;
-import java.util.Set;
+//import java.util.List;
+//import java.util.Set;
 
 @Entity
 @Table(name="docentt")
 public class Teacher extends User {
 
 
-    @Transient
-    private final String COLUMN_ID = "idgebruiker";
-
-    @Transient
-    private final String PK_COLUMN_TASK = "task_id";
+//    @Transient
+//    private final String COLUMN_ID = "idgebruiker";
+//
+//    @Transient
+//    private final String PK_COLUMN_TASK = "task_id";
 
     @Id
     @Column(name="idGebruiker")
     private int id;
-
-    @Transient
-    private List<Teacher> teachers;
-
-    //Hybernate mapping of the tables 2× @ManyToOne with IncidentalAvalability & GlobalAvalability
-    @ManyToOne
-    @JoinColumn(name="dayParts")
-    @JoinColumn(name="incidenten")
-    public List<Teacher> getTeachers() { return teachers; }
+//
+//    @Transient
+//    private List<Teacher> teachers;
+//
+//    //Hybernate mapping of the tables 2× @ManyToOne with IncidentalAvalability & GlobalAvalability
+//    @ManyToOne
+//    @JoinColumn(name="dayParts")
+//    @JoinColumn(name="incidenten")
+//    public List<Teacher> getTeachers() { return teachers; }
 
     //no-args
     public Teacher() { }
@@ -43,25 +43,25 @@ public class Teacher extends User {
     public void setId(int id) {
         this.id = id;
     }
-
-    @ManyToMany(fetch=FetchType.EAGER)
-    @JoinTable(name="tasks_teacher", joinColumns = @JoinColumn(name=COLUMN_ID),
-            inverseJoinColumns = @JoinColumn(name=PK_COLUMN_TASK))
-    private List<Task> tasks;
-
-    public List<Task> getTasks() {
-        return tasks;
-    }
-
-    public void setTasks(List<Task> tasks) {
-        this.tasks = tasks;
-    }
-
-    //toString
-    @Override
-    public String toString() {
-        return "Docent{" +
-                "id=" + id +
-                '}';
-    }
+//
+//    @ManyToMany(fetch=FetchType.EAGER)
+//    @JoinTable(name="tasks_teacher", joinColumns = @JoinColumn(name=COLUMN_ID),
+//            inverseJoinColumns = @JoinColumn(name=PK_COLUMN_TASK))
+//    private List<Task> tasks;
+//
+//    public List<Task> getTasks() {
+//        return tasks;
+//    }
+//
+//    public void setTasks(List<Task> tasks) {
+//        this.tasks = tasks;
+//    }
+//
+//    //toString
+//    @Override
+//    public String toString() {
+//        return "Docent{" +
+//                "id=" + id +
+//                '}';
+//    }
 }
