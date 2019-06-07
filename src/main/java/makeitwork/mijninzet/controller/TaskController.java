b9fbfc49f8096a2ceaeecf2adc0fa37734230fa8
Merel <m.slingenberg@hotmail.nl>
2019-06-07 18:21:18 +0200

    compleet met task save

--- src/main/java/makeitwork/mijninzet/controller/TaskController.java
+++ src/main/java/makeitwork/mijninzet/controller/TaskController.java
@@ -1,78 +1,116 @@
 package makeitwork.mijninzet.controller;
 
-
 import makeitwork.mijninzet.model.Task;
+import makeitwork.mijninzet.model.User;
+import makeitwork.mijninzet.repository.TaskRepository;
 import makeitwork.mijninzet.repository.UserRepository;
+import makeitwork.mijninzet.service.VacatureService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import makeitwork.mijninzet.repository.TaskRepository;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.*;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Controller
-public class TaskController {
+        (value ="/teacher")
+public class TaskController extends AbstractController {
 
     @Autowired
     private TaskRepository taskRepository;
-    @Autowired
-    private UserRepository userRepository;
 
-    public TaskController(TaskRepository taskRepository){
-        this.taskRepository = taskRepository;
-    }
+    @Autowired
+    private UserRepository usersRepository;
+
+    @Autowired
+    VacatureService vacatureService;
 
     @GetMapping("/taskOverview") //th:action
-    public String MenuHandler(Model model){
-        model.addAttribute("allTasks", tasks2Present());
+    public String MenuHandler(Model model, Principal principal) {
+        User user = usersRepository.findByUsername(principal.getName());
+        model.addAttribute("allTasks", tasks2React(user));
         return "taskOverview";
     }
+
     @GetMapping("/showTask/{task}")  //th:action
     public String TaskDetailHandler(@ModelAttribute("task") Task taak, @RequestParam("taskId") String taakId, Model model) {
-        taak=opening(taakId);
-        model.addAttribute("taak",taak);
+        taak = opening(taakId);
+        model.addAttribute("taak", taak);
         return "showTask"; //html
     }
 
-    private List<Task> tasks2Present(){
-//        alle taken uit database ophalen
+    @GetMapping("/taskSave/{taskId}") //mapping bij voor de view
+    public String ApplicationHandler(@ModelAttribute("task") Task taak, @RequestParam("taskId") String taakId, Principal principal) {
+        taak = opening(taakId);
+        vacatureService.addTask(taak, usersRepository.findByUsername(principal.getName()));
+        return "redirect:/taskOverview";
+    }
+
+    //haalt alles uit database
+    private List<Task> allTasks() {
         List<Task> tasks = this.taskRepository.findAll();
-//        de taken verwijderen waar user eerder op reageerde
-        tasks2React(tasks);
-//        taken op alfabetische volgorde zetten
         sortTasks(tasks);
-//        opgeschoonde lijst aan handler geven
         return tasks;
     }
 
-    private Task opening(String taskId){
+    private Task opening(String taskId) {
         return this.taskRepository.findDocumentById(taskId);
     }
-    private List<Task> sortTasks(List<Task> tasks){
-        Collections.sort(tasks,(a,b) -> {return a.getTitel().compareTo(b.getTitel());});
+
+    private List<Task> sortTasks(List<Task> tasks) {
+        Collections.sort(tasks, (a, b) -> {
+            return a.getTitel().compareTo(b.getTitel());
+        });
         return tasks;
     }
-    private List<Task> tasks2React(List<Task> tasks){
-        //todo taken waar <user> eerder op reageerde uit database halen
-        Set<Task> reacted = new HashSet<>();
-//        statement hieronder verder uitwerken
-//        set<Tasks> reacted = this.reactedrepo.findbyUser(<currentUserName()>
-        Set<Task> alleVacatures= new HashSet<>(tasks);
-        alleVacatures.removeAll(reacted); // levert tasks op waaruit alle eerder gereageerde taken zijn verwijderd
-        List<Task> tasks2Do =new ArrayList<>(alleVacatures);
-        return tasks2Do;
+
+    //lijst met vacatures van Docent (zit ook al in Service)
+//    private Set<Application> findApplicationByUsername(User user){
+//        Set<Application> applications = sollicitatieRepository.findAllByUser(user);
+//        return applications;
+//    }
+
+    //lijst met taken waar de docent nog op kan reageren
+    private List<Task> tasks2React(User user) {
+        List<Task> tasks = allTasks();
+        System.out.println("All: " + tasks);
+        List<Task> myTasks = vacatureService.getAllTasks(user);
+        System.out.println("My tasks: " + myTasks);
+        List<Task> possibleTasks = new ArrayList<>();
+        for (Task t : tasks) {
+            if (!isContained(t, myTasks)) {
+                possibleTasks.add(t);
+            }
+        }
+        System.out.println(possibleTasks);
+        return possibleTasks;
     }
-    private String currentUserName(){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (!(authentication instanceof AnonymousAuthenticationToken)) {
-            return authentication.getName();
-        } else return "";
+
+
+
+    public boolean isContained(Task t, List<Task> listTask) {
+        for (Task t2 : listTask) {
+            if (t.getId().equals(t2.getId())) {
+                return true;
+            }
+        }
+        return false;
     }
-//
+}
+
+
+//    private String currentUserName(){
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+//            return authentication.getName();
+//        } else return "";
+//    }
+////
 //    @PutMapping
 //    public void insert(@RequestBody Task task){
 //        this.taskRepository.insert(task);
@@ -85,9 +123,9 @@ public class TaskController {
 
 //    @DeleteMapping("/{id}")
 //    public void delete(@PathVariable ("id") String id){
-//        this.taskRepository.deleteById(id);
-//
-//    }
+//        this.taskRepository.deleteById(id)
+// }
+//        tasks.forEach((i)->myTasks.remove(i));
+////         tasks.removeAll(myTasks); // levert tasks op waaruit alle eerder gereageerde taken zijn verwijderd
+//        List<Task> diff = tasks.stream().filter(e -> !myTasks.contains(e)).collect(Collectors.toList());
 
-
-}
--- src/main/java/makeitwork/mijninzet/repository/VacatureRepository.java
+++ src/main/java/makeitwork/mijninzet/repository/VacatureRepository.java
@@ -1,11 +1,11 @@
 package makeitwork.mijninzet.repository;
 
-import makeitwork.mijninzet.model.Teacher;
+import makeitwork.mijninzet.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VacatureRepository extends JpaRepository<Teacher, Integer> {
+public interface VacatureRepository extends JpaRepository<User, Integer> {
 
 }
 
