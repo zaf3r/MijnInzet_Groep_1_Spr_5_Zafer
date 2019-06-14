9a79002bf2ee8f80f14599a4df8ba9057d34a636
David Heeneman <davidheeneman@gmail.com>
2019-06-14 14:43:24 +0200

    delete button improvement

--- src/main/java/makeitwork/mijninzet/controller/ManagerController.java
+++ src/main/java/makeitwork/mijninzet/controller/ManagerController.java
@@ -99,7 +99,7 @@ public class ManagerController {
     //in knowledgeAreaController
     @RequestMapping(value = "saveKnowledgeArea", method = RequestMethod.POST)
     public String saveArea(@ModelAttribute("saveKnowledgeArea") KnowledgeArea area, Model model) {
-        model.addAttribute("knowledgeArea", area.getKnowledgeArea(knowledgeAreaId));
+        model.addAttribute("knowledgeArea", area.getKnowledgeArea());
         areaRepo.save(area);
         return "redirect:/manager/kennisgebied";
     }
