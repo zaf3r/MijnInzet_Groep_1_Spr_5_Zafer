1bbabd89baa70f15264f6df0ba78ebc7dc221ac6
PeterP <peter.post@hva.nl>
2019-06-16 21:16:40 +0200

    KnowledgeArea aanpassing

--- src/main/java/makeitwork/mijninzet/controller/SubjectScheduleController.java
+++ src/main/java/makeitwork/mijninzet/controller/SubjectScheduleController.java
@@ -1,6 +1,7 @@
 package makeitwork.mijninzet.controller;
 
 import com.mongodb.BasicDBObject;
+import makeitwork.mijninzet.model.KnowledgeArea;
 import makeitwork.mijninzet.repository.KnowledgeAreaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,15 +17,16 @@ public class SubjectScheduleController {
 
     @PostMapping("/supplyKnowledgearea")
     public @ResponseBody String supplyKnowledgeAreas() {
-        var areas=knowledgeAreaRepository.findAll().sort();
+        var areas=knowledgeAreaRepository.findAll();
+        areas.sort(KnowledgeArea::compareTo);
 
 
         var output = new BasicDBObject();
-        if (eAdress != null) {
-            output.put("exists", true);
-        } else {
-            output.put("exists", false);
-        }
+//        if (eAdress != null) {
+//            output.put("exists", true);
+//        } else {
+//            output.put("exists", false);
+//        }
         return output.toJson();
     }
 }
--- src/main/java/makeitwork/mijninzet/model/KnowledgeArea.java
+++ src/main/java/makeitwork/mijninzet/model/KnowledgeArea.java
@@ -7,12 +7,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-public class KnowledgeArea {
+@Table(name = "studierichting")
+public class KnowledgeArea implements Comparable<KnowledgeArea> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int knowledgeAreaId;
 
+    @Column (name="richting")
     private String knowledgeArea;
 
 //    @OneToMany(mappedBy = "knowledgeArea", cascade= {CascadeType.PERSIST,CascadeType.REMOVE})
@@ -34,4 +36,19 @@ public class KnowledgeArea {
 
 //    public Set<Subject> getSubjectSet() { return subjectSet; }
 //    public void setSubjectSet(Set<Subject> subjectSet) { this.subjectSet = subjectSet; }
+
+    @Transient
+    @Override
+    public int compareTo(KnowledgeArea o) {
+        return o.getKnowledgeArea().compareTo(this.getKnowledgeArea());
+    }
+
+    @Transient
+    @Override
+    public String toString() {
+        return "KnowledgeArea{" +
+                "knowledgeAreaId=" + knowledgeAreaId +
+                ", studierichting='" + knowledgeArea + '\'' +
+                '}';
+    }
 }
