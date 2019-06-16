a9bdddf4e3578e82c4ae5535b6b18b155f0eb61e
PeterP <peter.post@hva.nl>
2019-06-16 20:51:47 +0200

    cruduser functio

---
+++ src/main/java/makeitwork/mijninzet/controller/SubjectScheduleController.java
@@ -0,0 +1,30 @@
+package makeitwork.mijninzet.controller;
+
+import com.mongodb.BasicDBObject;
+import makeitwork.mijninzet.repository.KnowledgeAreaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SubjectScheduleController {
+
+    @Autowired
+    KnowledgeAreaRepository knowledgeAreaRepository;
+
+    @PostMapping("/supplyKnowledgearea")
+    public @ResponseBody String supplyKnowledgeAreas() {
+        var areas=knowledgeAreaRepository.findAll().sort();
+
+
+        var output = new BasicDBObject();
+        if (eAdress != null) {
+            output.put("exists", true);
+        } else {
+            output.put("exists", false);
+        }
+        return output.toJson();
+    }
+}
