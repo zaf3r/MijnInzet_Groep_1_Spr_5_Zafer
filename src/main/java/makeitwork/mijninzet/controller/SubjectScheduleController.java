1d4813b980776800f9fa25eaab47e43fe5eaa93f
PeterP <peter.post@hva.nl>
2019-06-17 11:23:43 +0200

    subjectSchedule kleine aanpassingen

--- src/main/java/makeitwork/mijninzet/controller/SubjectScheduleController.java
+++ src/main/java/makeitwork/mijninzet/controller/SubjectScheduleController.java
@@ -1,5 +1,6 @@
 package makeitwork.mijninzet.controller;
 
+import com.google.gson.Gson;
 import com.mongodb.BasicDBObject;
 import makeitwork.mijninzet.model.KnowledgeArea;
 import makeitwork.mijninzet.repository.KnowledgeAreaRepository;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class SubjectScheduleController {
 
@@ -17,16 +21,14 @@ public class SubjectScheduleController {
 
     @PostMapping("/supplyKnowledgearea")
     public @ResponseBody String supplyKnowledgeAreas() {
+//        alle kennisgebieden worden opgehaald en gesorteerd
         var areas=knowledgeAreaRepository.findAll();
         areas.sort(KnowledgeArea::compareTo);
-
-
-        var output = new BasicDBObject();
-//        if (eAdress != null) {
-//            output.put("exists", true);
-//        } else {
-//            output.put("exists", false);
-//        }
-        return output.toJson();
+        List<String> areaNames=new ArrayList<>();
+        for (KnowledgeArea area:areas
+             ) {areaNames.add(area.getKnowledgeArea());
+        }
+        String payload=new Gson().toJson(areaNames);
+        return payload;
     }
 }
