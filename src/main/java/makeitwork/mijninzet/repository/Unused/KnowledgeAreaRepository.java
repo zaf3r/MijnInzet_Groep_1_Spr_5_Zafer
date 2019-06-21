74aff930c5fefd09065d2856ccb5438c775b67ea
Z.S. Torun <torunzafer@hotmail.com>
2019-06-21 13:37:44 +0200

    - fixed bug in preference view

--- src/main/java/makeitwork/mijninzet/repository/Unused/KnowledgeAreaRepository.java
+++ src/main/java/makeitwork/mijninzet/repository/Unused/KnowledgeAreaRepository.java
@@ -1,12 +1,9 @@
-package makeitwork.mijninzet.model.Unused;
+package makeitwork.mijninzet.repository.Unused;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
+/*
 public interface KnowledgeAreaRepository extends JpaRepository<KnowledgeArea, Integer> {
     List<KnowledgeArea> findAllByOrderByKnowledgeAreaIdAsc();
     KnowledgeArea findByKnowledgeAreaId(int id);
 }
+*/
+
