189ab0e9230f66a17174bdeaec9e83e8667dbd78
PeterP <peter.post@hva.nl>
2019-06-22 12:17:44 +0200

    wijziging om te zorgen dat applicatie compiled

--- src/main/java/makeitwork/mijninzet/controller/SubjectScheduleController.java
+++ src/main/java/makeitwork/mijninzet/controller/SubjectScheduleController.java
@@ -1,17 +1,33 @@
-//package makeitwork.mijninzet.controller;
-//
-//import com.mongodb.BasicDBObject;
-//import makeitwork.mijninzet.repository.KnowledgeAreaRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//@Controller
-//public class SubjectScheduleController {
-//
-//    @Autowired
-//    KnowledgeAreaRepository knowledgeAreaRepository;
-//
-//}
+package makeitwork.mijninzet.controller;
+
+import com.google.gson.Gson;
+import makeitwork.mijninzet.model.Cohort;
+import makeitwork.mijninzet.repository.CohortRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class SubjectScheduleController {
+
+    @Autowired
+    CohortRepository cohortRepository;
+
+    @PostMapping("/cohortsToPlan")
+    public @ResponseBody
+    String allRoles(@RequestBody String requestPayload){
+        List<Cohort> cohortsPlan=new ArrayList<>();
+
+        Gson output= new Gson();
+        return output.toJson(cohortsPlan);
+    }
+//    private List<Cohort>
+
+
+
+}
