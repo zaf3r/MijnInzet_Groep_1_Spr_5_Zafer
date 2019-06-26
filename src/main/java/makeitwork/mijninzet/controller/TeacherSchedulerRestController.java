ad676e18ead2380e7aa5f4b54f10fb27c0b16750
Z.S. Torun <torunzafer@hotmail.com>
2019-06-26 22:21:59 +0200

    - reversed default color in incidentform

--- src/main/java/makeitwork/mijninzet/controller/TeacherSchedulerRestController.java
+++ src/main/java/makeitwork/mijninzet/controller/TeacherSchedulerRestController.java
@@ -6,10 +6,7 @@ import makeitwork.mijninzet.model.TeacherSchedule.CohortWeek;
 import makeitwork.mijninzet.repository.CohortRepository;
 import makeitwork.mijninzet.repository.CohortWeekRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,7 +21,7 @@ public class TeacherSchedulerRestController {
     @Autowired
     CohortRepository cohortRepository;
 
-    @GetMapping("cohort/weeks/{cohortName}")
+    @GetMapping("/cohort/weeks/{cohortName}")
     public List<Integer> findCohortWeeksHandler(@PathVariable("cohortName") String cohortNaam) {
         Cohort cohort = cohortRepository.findByCohortName(cohortNaam);
         List<CohortWeek> cohortWeekList = cohortWeekRepository.findCohortWeekByCohort(cohort);
@@ -41,7 +38,7 @@ public class TeacherSchedulerRestController {
         return listOfWeekNumbers;
     }
 
-    @GetMapping("cohort/days/{cohortName}/{cohortWeek}")
+    @GetMapping("/cohort/days/{cohortName}/{cohortWeek}")
     public List<CohortDay> findCohortDayData(@PathVariable("cohortName") String cohortName,
                                              @PathVariable("cohortWeek") int cohortWeekNumber) {
 
@@ -50,4 +47,11 @@ public class TeacherSchedulerRestController {
         return cohortWeek.getCohortDayList();
     }
 
+//    @PostMapping("/saveSubjectSchule")
+//    public void saveIncidentListHandler(/*@RequestBody DTO TO DO*/) {
+//
+//        formDataLoader(incidentList, incidentJsonHolder);
+//
+//        incidentRepository.saveAll(incidentList);
+//    }
 }
