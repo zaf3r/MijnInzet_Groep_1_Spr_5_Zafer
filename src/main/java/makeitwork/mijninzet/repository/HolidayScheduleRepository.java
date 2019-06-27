a487f05106c9bd2456037ee6038221fc93d167c2
PeterP <peter.post@hva.nl>
2019-06-27 09:49:48 +0200

    Work in Progress subjectSchedule

--- src/main/java/makeitwork/mijninzet/repository/HolidayScheduleRepository.java
+++ src/main/java/makeitwork/mijninzet/repository/HolidayScheduleRepository.java
@@ -4,6 +4,10 @@ import makeitwork.mijninzet.model.HolidaySchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface HolidayScheduleRepository extends JpaRepository<HolidaySchedule, LocalDate> {
+    Boolean findByLocalDate(LocalDate localDate);
+
+    List<HolidaySchedule> findAllByLocalDateAfter(LocalDate localDate);
 }
