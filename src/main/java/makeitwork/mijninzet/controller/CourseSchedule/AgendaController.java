0e0b33578c555bb877799f1525750f97efdf9be9
PeterP <peter.post@hva.nl>
2019-06-30 11:35:54 +0200

    last part of previous

--- src/main/java/makeitwork/mijninzet/controller/CourseSchedule/AgendaController.java
+++ src/main/java/makeitwork/mijninzet/controller/CourseSchedule/AgendaController.java
@@ -5,6 +5,8 @@ import makeitwork.mijninzet.model.CourseSchedule.receiveBeginClosingDate;
 import makeitwork.mijninzet.service.CourseSchedule.CourseScheduleAgenda;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,9 +15,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class AgendaController {
     @Autowired
     CourseScheduleAgenda agenda;
-    @Autowired
 
-
+    @GetMapping("/test1")
+    public String agendaFrontEnd(Model model){
+        System.out.printf("Dit is test1/courseAgenda");
+        return "CourseSchedule/courseAgenda";
+    }
 
     @PostMapping("/agenda")
     public @ResponseBody
